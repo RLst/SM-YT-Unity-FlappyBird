78d70ae616f841faa80c6ba6c5341cb74edc163f
STL8SE\Shader Motion <shader.huyle@gmail.com>
2023-04-15 02:57:40 +0700

    UD0CE8.3: Handling death, bird, hazards

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     public float flyForce = 5f;
+    public bool isDead = false;
     Rigidbody2D rb;
 
-    // Start is called before the first frame update
     void Start()
     {
+        isDead = false;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * flyForce;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Hazard>())
+        {
+            isDead = true;  //DEATH!
         }
     }
 }
---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    //This class does nothing and is only being used as a tag
+}
